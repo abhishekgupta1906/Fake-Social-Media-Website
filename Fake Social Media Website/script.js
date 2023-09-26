dd4fbba3f1f607f10c23d28c5833f8ca2338a247
abhishekgupta1906 <122986855+abhishekgupta1906@users.noreply.github.com>
2023-09-26 19:38:30 +0530

    First upload

---
+++ Fake Social Media Website/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+function openTab(tabId) {
+  const tabs = document.querySelectorAll(".tab-content");
+  tabs.forEach((tab) => tab.classList.add("hidden"));
+  document.getElementById(tabId).classList.remove("hidden");
+}
